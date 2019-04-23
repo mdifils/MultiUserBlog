9c71c19e4f4cd3accbe77d6910dce44a35b2b53f
Michel Difils <michel.fils@hotmail.com>
2019-04-23 17:17:05 +0200

    Writting Post views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
+
+from django.views.generic import (TemplateView,ListView,
+                                  DetailView,CreateView,
+                                  UpdateView,DeleteView)
+
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'blog/about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # SELECT * FROM post WHERE published_date <= timezone.now()
+        # '-published_date' means ordered by descending order
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+
+    form_class = PostForm
+
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    # reverse_lazy allows to wait until the post is deleted before redirecting
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_draft_list.html'
+
+    model = Post
+
+    def get_queryset(self):
+        # __isnull means there is no published_date
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
